8a85f69d5af6f3c4eb8b1ab74f15a7f59e161a9d
Zundux <zundux@gmail.com>
2017-05-18 09:12:23 +0700

    rename main file to app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "babel-polyfill";
 import "core-js/fn/object/assign";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/Main";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
